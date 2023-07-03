606f93dd1d4cd009833f4c46908ad9f85506f9c0
PRABHANJAN <ranjank.42099@gmail.com>
2023-07-03 16:34:42 +0530

    ddd

---
+++ src/main/java/com/prabhanjan/service/ServiceClass2.java
@@ -0,0 +1,18 @@
+package com.prabhanjan.service;
+
+public class ServiceClass2 {
+	private String name;
+
+	private String rollout;
+	private boolean isDevelopr;
+
+	public String getString()
+	{
+		String n1;
+		return "Prabhanjan";
+		
+	}
+	public void display() {
+		System.out.println("Hello PK");
+	}
+}
